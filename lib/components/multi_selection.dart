c22cf8e88004e82344151e7a2577bfb6b5aba865
SirOtaiv <rauotavio7@gmail.com>
2026-05-22 21:48:00 -0300

    fix: Corrigido nome de classe para primeira letra maiuscula

--- lib/components/multi_selection.dart
+++ lib/components/multi_selection.dart
@@ -1,44 +1,44 @@
 import 'package:flutter/material.dart';
 
-class multiSelection extends StatefulWidget {
+class MultiSelection extends StatefulWidget {
   final List<String> labels;
   final Function(List<String> selecionados) onChange;
 
-  const multiSelection({
+  const MultiSelection({
     super.key,
     required this.labels,
     required this.onChange,
   });
 
   @override
-  State<multiSelection> createState()=>_MultiSelectionState();
-  
+  State<MultiSelection> createState() => _MultiSelectionState();
 }
-class _MultiSelectionState extends State<multiSelection>{
-  final List<String> selecionados=[];
+
+class _MultiSelectionState extends State<MultiSelection> {
+  final List<String> selecionados = [];
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        for(var label in widget.labels)
+        for (var label in widget.labels)
           CheckboxListTile(
             title: Text(label),
             controlAffinity: ListTileControlAffinity.leading,
             value: selecionados.contains(label),
-            onChanged: (bool? marcado){
+            onChanged: (bool? marcado) {
               setState(() {
-                if(marcado==true){
+                if (marcado == true) {
                   selecionados.add(label);
-                }else{
+                } else {
                   selecionados.remove(label);
                 }
               });
 
               widget.onChange(List.from(selecionados));
             },
-          )
+          ),
       ],
     );
   }
