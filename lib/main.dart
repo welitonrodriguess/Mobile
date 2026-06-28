350cbb3bbe45d51a959b48767ab178f7a88e96ad
SirOtaiv <rauotavio7@gmail.com>
2026-06-28 16:16:15 -0300

    fix: Corrigido arquivos para aprovação de PR

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:dev_venture/screens/theme_demo.dart';
 import 'package:dev_venture/screens/activities_screen.dart';
 import 'package:dev_venture/screens/cadastro_screen.dart';
 import 'package:dev_venture/screens/login_screen.dart';
+import 'package:dev_venture/screens/ranking_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/screens/ranking_screen.dart
+++ lib/screens/ranking_screen.dart
@@ -2,23 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RankingScreen extends StatelessWidget {
-  const RankingScreen({Key? key}) : super(key: key);
+  const RankingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Ranking Global'),
-      ),
-      
+      appBar: AppBar(title: const Text('Ranking Global')),
+
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance
             .collection('usuarios')
             .orderBy('pontuacao', descending: true)
             .snapshots(),
-            
+
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          
           if (snapshot.hasError) {
             return const Center(
               child: Text('Ocorreu um erro ao carregar o ranking.'),
@@ -26,9 +23,7 @@ class RankingScreen extends StatelessWidget {
           }
 
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
+            return const Center(child: CircularProgressIndicator());
           }
 
           if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
@@ -42,15 +37,14 @@ class RankingScreen extends StatelessWidget {
           return ListView.builder(
             itemCount: documentos.length,
             itemBuilder: (context, index) {
-              Map<String, dynamic> data = documentos[index].data() as Map<String, dynamic>;
-              
+              Map<String, dynamic> data =
+                  documentos[index].data() as Map<String, dynamic>;
+
               String nomeJogador = data['nome'] ?? 'Jogador Anônimo';
               int pontuacao = data['pontuacao'] ?? 0;
 
               return ListTile(
-                leading: CircleAvatar(
-                  child: Text('${index + 1}º'),
-                ),
+                leading: CircleAvatar(child: Text('${index + 1}º')),
                 title: Text(
                   nomeJogador,
                   style: const TextStyle(fontWeight: FontWeight.bold),
